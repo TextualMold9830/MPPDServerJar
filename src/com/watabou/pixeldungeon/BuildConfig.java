c2a1a03164fd206fdf310bf6384874d0af7f541b
TextualMold9830 <88786053+TextualMold9830@users.noreply.github.com>
2024-11-11 15:31:12 +0200

    Disable the event logging by default

--- src/com/watabou/pixeldungeon/BuildConfig.java
+++ src/com/watabou/pixeldungeon/BuildConfig.java
@@ -2,5 +2,5 @@ package com.watabou.pixeldungeon;
 
 public class BuildConfig {
     public static boolean DEBUG = true;
-    public static boolean EVENT_LOGGING = true;
+    public static boolean EVENT_LOGGING = false;
 }
